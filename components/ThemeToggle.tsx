6544783b781b0c119c2567d2fb28576093a136cb
Quangminh_24112005 <phamlequangminh2411@gmail.com>
2026-09-24 16:25:01 +0700

    Fix moon theme icon orientation

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -34,7 +34,7 @@ export default function ThemeToggle() {
         className="flex h-9 w-9 items-center justify-center border-2 border-black bg-white text-blue-600 shadow-[2px_2px_0px_#000000] transition-transform hover:-translate-x-0.5 hover:-translate-y-0.5 active:translate-x-0.5 active:translate-y-0.5 dark:border-blue-400 dark:bg-zinc-900 dark:text-blue-400 dark:shadow-[2px_2px_0px_#000000] dark:hover:shadow-[3px_3px_0px_#000000]"
       >
         <span className="flex">
-          {theme === "dark" ? <SunIcon className="h-4 w-4" /> : <MoonIcon className="h-4 w-4 rotate-180" />}
+          {theme === "dark" ? <SunIcon className="h-4 w-4" /> : <MoonIcon className="h-4 w-4" />}
         </span>
       </button>
     </>
